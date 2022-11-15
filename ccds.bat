81daf37c350d551ce806a6975b472d4cc78eafaa
riyapawar <73252110+riyapawar@users.noreply.github.com>
2022-11-14 21:46:22 -0500

    Delete ccds.bat

--- ccds.bat
+++
@@ -1,4 +0,0 @@
-git clone https://github.com/ltkhang/sdn-ids-ddos-defense.git 
-git clone https://github.com/nttdots/go-dots.git
-git clone https://github.com/samvid25/ARP-Spoofing-Detection-and-Prevention.git
-git clone https://github.com/AlphaDelta/Secure-Desktop.git
