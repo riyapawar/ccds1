ba563084f74a5a4f424aacbdc1e715d8dcfa6470
riyapawar <73252110+riyapawar@users.noreply.github.com>
2022-11-14 21:43:10 -0500

    Create ccds.bat

---
+++ ccds.bat
@@ -0,0 +1,4 @@
+git clone https://github.com/ltkhang/sdn-ids-ddos-defense.git 
+git clone https://github.com/nttdots/go-dots.git
+git clone https://github.com/samvid25/ARP-Spoofing-Detection-and-Prevention.git
+git clone https://github.com/AlphaDelta/Secure-Desktop.git
