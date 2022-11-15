71b38deea578c1b8c9c0bcbdfde8e35e8c832aa0
riyapawar <73252110+riyapawar@users.noreply.github.com>
2022-11-14 21:50:23 -0500

    Update ccds.bat

--- ccds.bat
+++ ccds.bat
@@ -1,5 +1,5 @@
 @ECHO OFF 
-git clone https://github.com/ltkhang/sdn-ids-ddos-defense.git 
-git clone https://github.com/nttdots/go-dots.git
-git clone https://github.com/samvid25/ARP-Spoofing-Detection-and-Prevention.git
-git clone https://github.com/AlphaDelta/Secure-Desktop.git
+git clone https://github.com/ltkhang/sdn-ids-ddos-defense.git REM running SDN Controller 
+git clone https://github.com/nttdots/go-dots.git REM running DOTS implementation
+git clone https://github.com/samvid25/ARP-Spoofing-Detection-and-Prevention.git REM running ARP proj
+git clone https://github.com/AlphaDelta/Secure-Desktop.git REM running Secure Desktop proj
