9c1688895172ed204cddf13163b287e6cd23020a
riyapawar <73252110+riyapawar@users.noreply.github.com>
2022-11-14 22:03:52 -0500

    Update ccds.bat

--- ccds.bat
+++ ccds.bat
@@ -3,3 +3,4 @@ git clone https://github.com/ltkhang/sdn-ids-ddos-defense.git REM running SDN Co
 git clone https://github.com/nttdots/go-dots.git REM running DOTS implementation
 git clone https://github.com/samvid25/ARP-Spoofing-Detection-and-Prevention.git REM running ARP proj
 git clone https://github.com/AlphaDelta/Secure-Desktop.git REM running Secure Desktop proj
+pause
